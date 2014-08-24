66e001f7ee5b47fbdb9858653e61138026b8cef3
Rahul Horé <hore.rahul@gmail.com>
2014-08-24 19:30:57 -0400

    Creates Tweet model

---
+++ app/models/tweet.rb
@@ -0,0 +1,8 @@
+class Tweet < ActiveRecord::Base
+  serialize :hashtags, Array
+  serialize :urls, Array
+  serialize :user_mentions, Array
+  serialize :tweeter_profile_image_url, Addressable::URI
+
+  validates_uniqueness_of :tweet_id
+end

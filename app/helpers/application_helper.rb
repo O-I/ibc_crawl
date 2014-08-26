9868a5c64836fb270c6cb9203c821df15428ebdb
Rahul Horé <hore.rahul@gmail.com>
2014-08-25 21:01:55 -0400

    Adds tweets index view and helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def sortable(link_name, column, direction = 'DESC')
+    link_to link_name, col: column, dir: direction
+  end
+
+  def shufflable(link_name)
+    link_to link_name, mix: true
+  end
 end
---
+++ app/helpers/tweets_helper.rb
@@ -0,0 +1,23 @@
+module TweetsHelper
+  def profile_pic(tweet)
+    image_tag tweet.tweeter_profile_image_url.to_s, class: 'fave-profile-pic'
+  end
+
+  def link_to_tweeter(tweet)
+    link_to tweet.tweeter_name, "https://twitter.com/#{tweet.tweeter_screen_name}", class: 'tweeter_link', target: '_blank'
+  end
+
+  def link_to_tweet_id(tweet)
+    link_to "#{time_ago_in_words tweet.tweet_date} ago",
+            "https://twitter.com/#{tweet.tweeter_screen_name}/status/#{tweet.tweet_id}",
+            class: 'tweet_link', target: '_blank'
+  end
+
+  def text_to_true_link(tweet_text)
+    urls = tweet_text.scan(/http\S*/)
+    urls.each do |url|
+      tweet_text.gsub!(url, "<a href=#{url} target='_blank'>#{url}</a>")
+    end
+    tweet_text.html_safe
+  end
+end

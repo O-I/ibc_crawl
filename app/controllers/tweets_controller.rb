785129f7bd594875e3ac44154ca469f050943bc7
Rahul Horé <hore.rahul@gmail.com>
2014-08-25 21:02:28 -0400

    Adds tweets controller

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,24 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.order(sort).page(params[:page])
+  end
+
+  private
+
+  def sort
+    shuffle? ? 'RANDOM()' : "#{sort_column} #{sort_direction}"
+  end
+
+  def sort_column
+    sortable_columns = %w(tweet_date favorite_count retweet_count)
+    sortable_columns.include?(params[:col]) ? params[:col] : 'tweet_date'
+  end
+
+  def sort_direction
+    %w(ASC DESC).include?(params[:dir]) ? params[:dir] : 'DESC'
+  end
+
+  def shuffle?
+    params[:mix] == 'true'
+  end
+end

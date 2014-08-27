4ed20aafd813adb9d6cb03dfc4738599f0abffd4
Rahul Horé <hore.rahul@gmail.com>
2014-08-26 21:32:01 -0400

    Adds pg_search_scope to Tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,4 +7,14 @@ class Tweet < ActiveRecord::Base
   validates_uniqueness_of :tweet_id
 
   self.per_page = 25
+
+  include PgSearch
+  pg_search_scope :seek,
+    against: { tweeter_name: 'A', tweeter_screen_name: 'B', tweet_text: 'C' },
+    using: { tsearch: { dictionary: 'english', prefix: true } },
+    ignoring: :accents
+
+  def self.search(query)
+    query.present? ? seek(query) : all
+  end
 end
